68517b85f58cbe908749b446a36402f382e41013
imdcln <declanecun@gmail.com>
2025-09-09 14:03:58 +0700

    Revision

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::name('signup.')->group( function() {
+    Route::get('/signup', [AuthController::class, 'step1'])->name('step1');
+    Route::get
+}
+);
+
+
